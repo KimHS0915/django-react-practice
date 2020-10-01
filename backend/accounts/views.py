7e5997fbe76f8b53b64863ddc05c7d4e9b09d2a9
KimHS0915 <remember2552@gmail.com>
2020-10-01 23:36:16 +0900

    Add sign up API

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import SignupSerializer
+
+class SignupView(CreateAPIView):
+    model = get_user_model()
+    serializer_class = SignupSerializer
+    permission_classes = [
+        AllowAny,
+    ]
