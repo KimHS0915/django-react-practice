d45e87816d1349b7703fa440935e1363c10ce066
KimHS0915 <remember2552@gmail.com>
2020-10-02 21:24:37 +0900

    JWT Token

---
+++ frontend/src/utils/LoginRequiredRoute.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useAppContext } from 'store';
+
+function LoginRequireRoute({ component: Component, ...kwargs }) {
+    const {
+        store: { isAuthenticated }
+    } = useAppContext();
+
+    if (isAuthenticated) {
+
+    }
+    else {
+
+    }
+
+    return (
+        <Route 
+            {...kwargs} 
+            render={props => {
+                if (isAuthenticated) {
+                    return (<Component {...props} />);
+                } else {
+                    return (
+                        <Redirect 
+                            to={{
+                                pathname: "/accounts/login", 
+                                state: { from: props.location }
+                            }}
+                        />
+                    );
+                }
+            }}
+       />
+    );
+}
+
+export default LoginRequireRoute;
