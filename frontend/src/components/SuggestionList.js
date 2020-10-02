c4bfbe0c4f3e9a8726d438d80f8911d2335d35a1
KimHS0915 <remember2552@gmail.com>
2020-10-02 22:21:09 +0900

    API request using axios-hooks

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { Card } from 'antd';
 import Suggestion from './Suggestion';
-import Axios from 'axios';
+import useAxios from 'axios-hooks';
 import { useAppContext } from 'store';
 import './SuggestionList.scss';
 
 export default function SuggestionList({ style }) {
     const { store: { jwtToken }} = useAppContext();
-    const [userList, setUserList] = useState([]);
 
-    useEffect(() => {
-        async function fetchUserList() {
-            const apiUrl = "http://localhost:8000/accounts/suggestions/";
-            const headers = { Authorization: `JWT ${jwtToken}` };
-            try {
-                const { data } = await Axios.get(apiUrl, { headers });
-                setUserList(data);
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        fetchUserList();
-    }, []);
+    const headers = { Authorization: `JWT ${jwtToken}` };
+    
+    const apiUrl = "http://localhost:8000/accounts/suggestions/";
+
+    const [{ data: userList, loading, error }, refetch] = useAxios({
+        url: apiUrl,
+        headers,
+    });
+
 
     return (
         <div style={style}>
+            {loading && <div>Loading</div>}
+            {error && <div>Error</div>}
+
+            <button onClick={() => refetch()}>Reload</button>
+            
             <Card title="Suggestions for you" size="small">
-                {userList.map(suggestionUser => (
-                    <Suggestion 
-                    key={suggestionUser.username}
-                    suggestionUser={suggestionUser} />
-                ))}
+                {userList &&
+                    userList.map(suggestionUser => (
+                        <Suggestion 
+                            key={suggestionUser.username}
+                            suggestionUser={suggestionUser} 
+                        />
+                    ))}
             </Card>            
         </div>
     );
